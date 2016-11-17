b2ce614a693a0778c33d3df5180ca2a2ab325f46
yurivan <yivanov25r@gmail.com>
2016-11-17 07:26:22 +0400

    Browser operations robustness improved.

--- src/main/java/ru/yurivan/selenium/litecart/managers/Browser.java
+++ src/main/java/ru/yurivan/selenium/litecart/managers/Browser.java
@@ -9,25 +9,34 @@ public class Browser {
     private static WebDriverWait defaultWait;
 
     public static void start() {
-        if (null == driver()) {
+        if (null == driver) {
             driver = new ChromeDriver();
             defaultWait = new WebDriverWait(driver, 10);
         }
     }
 
     public static void quit() {
-        if (null != driver()) {
-            driver().quit();
+        if (null != driver) {
+            driver.quit();
             driver = null;
+            defaultWait = null;
         }
     }
 
     public static WebDriver driver() {
+        ensureBrowserRunning();
         return driver;
     }
 
     public static WebDriverWait defaultWait() {
+        ensureBrowserRunning();
         return defaultWait;
+    }
+
+    private static void ensureBrowserRunning() {
+        if (null == driver) {
+            throw new IllegalStateException("Unable to complete operation. You should start browser first.");
+        }
     }
 }
 
